049d7d38538de8eb6eaee5a229865c8ff2115ba1
hassanwasfy <wasfyelgamed@gmail.com>
2023-04-29 12:49:50 +0300

    add generic success type

--- app/src/main/java/com/abaferastech/marvelapp/model/api/Success.kt
+++ app/src/main/java/com/abaferastech/marvelapp/model/api/Success.kt
@@ -1,8 +1,8 @@
 package com.abaferastech.marvelapp.model.api
 
-data class Success(
+data class Success<T>(
     override val code: Int,
     override val status: String,
     val etag: String,
-    val data: Data<Any?>
+    val data: Data<T>
 ) : ApiResponse()
