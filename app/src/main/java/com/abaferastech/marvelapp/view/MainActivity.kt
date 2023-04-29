cafe26438577f23cf6296cfb12614a60eb55499c
hassanwasfy <wasfyelgamed@gmail.com>
2023-04-29 17:39:41 +0300

    add glide dependencies

--- app/src/main/java/com/abaferastech/marvelapp/view/MainActivity.kt
+++ app/src/main/java/com/abaferastech/marvelapp/view/MainActivity.kt
@@ -2,10 +2,8 @@ package com.abaferastech.marvelapp.view
 
 import android.os.Bundle
 import android.os.PersistableBundle
-import android.view.LayoutInflater
 import androidx.appcompat.app.AppCompatActivity
-import com.abaferastech.marvelapp.BuildConfig
-import com.abaferastech.marvelapp.base.BaseActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
