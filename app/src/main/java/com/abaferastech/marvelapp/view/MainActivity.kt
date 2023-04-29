507931762ae364f73f88af64ea4ec6ffcd91f698
hassanwasfy <wasfyelgamed@gmail.com>
2023-04-29 18:52:04 +0300

    implment auto hash generator

--- app/src/main/java/com/abaferastech/marvelapp/view/MainActivity.kt
+++ app/src/main/java/com/abaferastech/marvelapp/view/MainActivity.kt
@@ -3,6 +3,9 @@ package com.abaferastech.marvelapp.view
 import android.os.Bundle
 import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
+import com.abaferastech.marvelapp.BuildConfig
+import java.security.MessageDigest
+import java.time.Instant
 
 
 class MainActivity : AppCompatActivity() {
@@ -10,4 +13,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState, persistentState)
 
     }
+}
+
+fun main() {
+    val timestamp = Instant.now().epochSecond.toString()
+    val privateKey = BuildConfig.pKey
+    val publicKey = BuildConfig.lKey
+    val inputString = timestamp + privateKey + publicKey
+    val md = MessageDigest.getInstance("MD5")
+    val hash = md.digest(inputString.toByteArray())
+
+    val hexString = hash.joinToString("") { "%02x".format(it) }
+    println(hexString)
 }
