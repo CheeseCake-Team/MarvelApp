65d475b6ba7f2dbe4fb958ce5f645ce7fe55e3a4
Abdurrahman <abdosalahas.01128@gmail.com>
2023-05-02 21:58:53 +0300

    implement base Adapter

--- app/src/main/java/com/abaferastech/marvelapp/ui/base/BaseAdapter.kt
+++ app/src/main/java/com/abaferastech/marvelapp/ui/base/BaseAdapter.kt
@@ -1,8 +1,32 @@
 package com.abaferastech.marvelapp.ui.base
 
-import androidx.databinding.ViewDataBinding
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
+import com.abaferastech.marvelapp.data.models.ResourceItem
 
-abstract class BaseAdapter<T,DB: ViewDataBinding>(): {
+abstract class BaseAdapter<T: ResourceItem>(
+    resourceItemDiffCallBack: BaseDiffUtilCallBack<T>
+) : ListAdapter<T, BaseViewHolder>(resourceItemDiffCallBack) {
+
+    abstract val layoutResourceId: Int
+
+    override fun onBindViewHolder(holder: BaseViewHolder, position: Int) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
+        BaseViewHolder(
+            DataBindingUtil.inflate(LayoutInflater.from(parent.context),
+                layoutResourceId, parent, false)
+        )
+
+}
+
+abstract class BaseDiffUtilCallBack<T: ResourceItem> : DiffUtil.ItemCallback<T>() {
+    override fun areItemsTheSame(oldItem: T, newItem: T) =
+        oldItem.id == newItem.id
 
 }
--- app/src/main/java/com/abaferastech/marvelapp/ui/base/BaseViewHolder.kt
+++ app/src/main/java/com/abaferastech/marvelapp/ui/base/BaseViewHolder.kt
@@ -1,8 +1,6 @@
 package com.abaferastech.marvelapp.ui.base
 
-import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
 import androidx.recyclerview.widget.RecyclerView
-import androidx.viewbinding.ViewBinding
 
-abstract class BaseViewHolder(binding: ViewDataBinding):RecyclerView.ViewHolder(binding.root)
+class BaseViewHolder(binding: ViewDataBinding):RecyclerView.ViewHolder(binding.root)
