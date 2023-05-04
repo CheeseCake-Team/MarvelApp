7b800e4140dcb7478793c944fbe50a2153a61ee7
Ziad mahmoud <ziaadmahmoud2001@hotmail.com>
2023-05-04 11:21:35 +0300

    make a characters fragment

---
+++ app/src/main/java/com/abaferastech/marvelapp/ui/characters/CharactersFragment.kt
@@ -0,0 +1,23 @@
+package com.abaferastech.marvelapp.ui.characters
+
+import android.os.Bundle
+import android.view.View
+import com.abaferastech.marvelapp.R
+
+import com.abaferastech.marvelapp.databinding.FragmentCharactersBinding
+import com.abaferastech.marvelapp.ui.base.BaseFragment
+
+class CharactersFragment:BaseFragment<FragmentCharactersBinding,CharactersViewModel>(){
+    override val layoutIdFragment: Int
+        get() = R.layout.fragment_characters
+    override val viewModelClass: Class<CharactersViewModel>
+        get() = CharactersViewModel::class.java
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+
+
+}
