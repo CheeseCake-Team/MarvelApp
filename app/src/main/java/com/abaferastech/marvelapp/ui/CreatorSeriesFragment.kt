45bd53c6080c41213a9d05556a2f186d6fb8a19b
Anwaar Raafat <raafatanwaar@gmail.com>
2023-05-04 23:13:00 +0300

    make creatoritem,creatorfragment

---
+++ app/src/main/java/com/abaferastech/marvelapp/ui/CreatorSeriesFragment.kt
@@ -0,0 +1,12 @@
+package com.abaferastech.marvelapp.ui.base
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.abaferastech.marvelapp.R
+
+class CreatorSeriesFragment : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_creator_series_fragment)
+    }
+}
