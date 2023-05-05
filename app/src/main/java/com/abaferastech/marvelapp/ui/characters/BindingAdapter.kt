b92af747c5bede99bd3d97af285c4ded58740301
Ziad mahmoud <ziaadmahmoud2001@hotmail.com>
2023-05-06 02:21:52 +0300

    make Binding adapter

---
+++ app/src/main/java/com/abaferastech/marvelapp/ui/characters/BindingAdapter.kt
@@ -0,0 +1,29 @@
+package com.abaferastech.marvelapp.ui.characters
+
+import android.widget.ImageView
+import androidx.databinding.BindingAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.abaferastech.marvelapp.data.model.Characters
+import com.abaferastech.marvelapp.data.model.Events
+import com.abaferastech.marvelapp.data.model.response.Thumbnail
+import com.abaferastech.marvelapp.ui.eventScreen.EventAdapter
+import com.bumptech.glide.Glide
+
+@BindingAdapter(value = ["app:imageUrl"])
+fun imageUrl (view: ImageView, thumbnail: Thumbnail?){
+    Glide.with(view)
+        .load("${thumbnail?.path}.${thumbnail?.extension}")
+        .centerCrop()
+        .into(view)
+}
+
+@BindingAdapter(value = ["app:items"])
+fun setRecyclerViewItems(view: RecyclerView, items: List<Characters>?){
+
+    if (items != null){
+        (view.adapter as CharactersAdpter).setItems(items)
+    }
+    else{
+        (view.adapter as CharactersAdpter).setItems(emptyList())
+    }
+}
