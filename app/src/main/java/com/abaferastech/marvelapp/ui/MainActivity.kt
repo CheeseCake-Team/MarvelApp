e967c2fdf9eeee5d53d8732a9591b2ef65cef0ef
Abdurrahman <abdosalahas.01128@gmail.com>
2023-05-05 18:33:39 +0300

    add FragmentContainerView update main activity update BaseFragment

--- app/src/main/java/com/abaferastech/marvelapp/ui/MainActivity.kt
+++ app/src/main/java/com/abaferastech/marvelapp/ui/MainActivity.kt
@@ -1,26 +1,19 @@
 package com.abaferastech.marvelapp.ui
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.ViewModelProvider
 import com.abaferastech.marvelapp.R
 import com.abaferastech.marvelapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private val viewModel: MarvelViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        init()
     }
-
-    private fun init() {
-        val viewModel = ViewModelProvider(this)[MarvelViewModel::class.java]
-        viewModel.series.observe(this) {
-            binding.text.text = it.toString()
-        }
-    }
-
 
 }
