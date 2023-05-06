634643157a5a50485b79ddf0a74ebb45137a62e3
Mujtaba Mohammed <mojtabamohamd92@gmail.com>
2023-05-06 17:38:12 +0300

    implement comics view all horizontal fragment

--- app/src/main/java/com/abaferastech/marvelapp/ui/comics/ComicsAdapter.kt
+++ app/src/main/java/com/abaferastech/marvelapp/ui/comics/ComicsAdapter.kt
@@ -2,20 +2,20 @@ package com.abaferastech.marvelapp.ui.comics
 
 import com.abaferastech.marvelapp.R
 import com.abaferastech.marvelapp.data.model.Comics
-import com.abaferastech.marvelapp.databinding.ItemComicBinding
+import com.abaferastech.marvelapp.databinding.ItemComicHorizontalBinding
 import com.abaferastech.marvelapp.ui.base.BaseAdapter
 import com.abaferastech.marvelapp.ui.base.BaseInteractionListener
 
 class ComicsAdapter(items: List<Comics>, listener: ComicsInteractionListener) :
     BaseAdapter<Comics>(items, listener) {
-    override val layoutID = R.layout.item_comic
+    override val layoutID = R.layout.item_comic_horizontal
 
     override fun bindItemViewHolder(
         holder: BaseViewHolder,
         item: Comics,
         listener: BaseInteractionListener?
     ) {
-        (holder.binding as ItemComicBinding).comics = item
+        (holder.binding as ItemComicHorizontalBinding).comicsHorizontal = item
     }
 }
 
--- app/src/main/java/com/abaferastech/marvelapp/ui/comics/ComicsViewAllHorizontalFragment.kt
+++ app/src/main/java/com/abaferastech/marvelapp/ui/comics/ComicsViewAllHorizontalFragment.kt
@@ -3,13 +3,13 @@ package com.abaferastech.marvelapp.ui.comics
 import android.os.Bundle
 import android.view.View
 import com.abaferastech.marvelapp.R
-import com.abaferastech.marvelapp.databinding.FragmentComicsViewAllBinding
+import com.abaferastech.marvelapp.databinding.FragmentComicsViewAllHorizontalBinding
 import com.abaferastech.marvelapp.ui.base.BaseFragment
 
-class ComicsViewAllFragment : BaseFragment<FragmentComicsViewAllBinding, ComicsViewModel>() {
+class ComicsViewAllHorizontalFragment : BaseFragment<FragmentComicsViewAllHorizontalBinding, ComicsViewModel>() {
 
     override val layoutIdFragment: Int
-        get() = R.layout.fragment_comics_view_all
+        get() = R.layout.fragment_comics_view_all_horizontal
 
     override val viewModelClass: Class<ComicsViewModel>
         get() = ComicsViewModel::class.java
