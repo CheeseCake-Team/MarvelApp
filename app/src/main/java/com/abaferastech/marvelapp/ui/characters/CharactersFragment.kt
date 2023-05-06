70ac410dbdd1c4b06854ff47034098d361a639e3
Abdurrahman <abdosalahas.01128@gmail.com>
2023-05-06 16:22:54 +0300

    fix characters fragment

--- app/src/main/java/com/abaferastech/marvelapp/ui/characters/CharactersFragment.kt
+++ app/src/main/java/com/abaferastech/marvelapp/ui/characters/CharactersFragment.kt
@@ -3,10 +3,9 @@ package com.abaferastech.marvelapp.ui.characters
 import android.os.Bundle
 import android.view.View
 import com.abaferastech.marvelapp.R
+import com.abaferastech.marvelapp.data.model.Characters
 import com.abaferastech.marvelapp.databinding.FragmentCharactersBinding
 import com.abaferastech.marvelapp.ui.base.BaseFragment
-import com.abaferastech.marvelapp.ui.eventScreen.EventAdapter
-import com.abaferastech.marvelapp.ui.eventScreen.EventsInteractionListener
 
 class CharactersFragment : BaseFragment<FragmentCharactersBinding, CharactersViewModel>() {
     override val layoutIdFragment: Int
@@ -17,7 +16,11 @@ class CharactersFragment : BaseFragment<FragmentCharactersBinding, CharactersVie
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val adapter = CharactersAdapter(emptyList(), object : CharactersInteractionListener {})
+        val adapter = ViewAllCharactersAdapter(emptyList(), object : CharactersInteractionListener {
+            override fun onCharacterClick(character: Characters) {
+
+            }
+        })
         binding.recyclerViewCharacters.adapter = adapter
     }
 }
--- app/src/main/java/com/abaferastech/marvelapp/ui/characters/CharactersViewModel.kt
+++ app/src/main/java/com/abaferastech/marvelapp/ui/characters/CharactersViewModel.kt
@@ -1,11 +1,9 @@
 package com.abaferastech.marvelapp.ui.characters
 
-import android.annotation.SuppressLint
 import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.abaferastech.marvelapp.data.model.Characters
-import com.abaferastech.marvelapp.data.model.Events
 import com.abaferastech.marvelapp.data.model.response.MarvelResponse
 import com.abaferastech.marvelapp.data.model.state.State
 import com.abaferastech.marvelapp.data.repository.MarvelRepository
@@ -13,18 +11,21 @@ import com.abaferastech.marvelapp.ui.base.BaseViewModel
 import io.reactivex.rxjava3.kotlin.addTo
 
 class CharactersViewModel : BaseViewModel() {
+    private val repository = MarvelRepository()
+
     init {
         getAllCharacters()
     }
 
-    val repository = MarvelRepository()
-    val _characters = MutableLiveData<List<Characters>>()
+    private val _characters = MutableLiveData<List<Characters>>()
 
 
     val characters: LiveData<List<Characters>> get() = _characters
 
-    fun getAllCharacters() {
-        repository.getAllCharacters().subscribe(::onSuccess, ::onError).addTo(compositeDisposable)
+    private fun getAllCharacters() {
+        repository.getAllCharacters()
+            .subscribe(::onSuccess, ::onError)
+            .addTo(compositeDisposable)
     }
 
     private fun onSuccess(state: State<MarvelResponse<Characters>>) {
