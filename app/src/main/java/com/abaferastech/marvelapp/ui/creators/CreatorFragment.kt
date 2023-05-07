f4487d92cce755ba18f7e8a03d49b6bb36745a11
Nahla <nenoopa@gmail.com>
2023-05-07 18:01:31 +0300

    add creator adapter

--- app/src/main/java/com/abaferastech/marvelapp/ui/creators/CreatorFragment.kt
+++ app/src/main/java/com/abaferastech/marvelapp/ui/creators/CreatorFragment.kt
@@ -1,18 +1,13 @@
 package com.abaferastech.marvelapp.ui.creators
 
 import android.os.Bundle
-import android.util.Log
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import com.abaferastech.marvelapp.R
 import com.abaferastech.marvelapp.databinding.CreatorSeriesFragmentBinding
 import com.abaferastech.marvelapp.ui.base.BaseFragment
-import com.abaferastech.marvelapp.ui.eventScreen.EventAdapter
-import com.abaferastech.marvelapp.ui.eventScreen.EventsInteractionListener
 
 
-class CreatorsFragment : BaseFragment<CreatorSeriesFragmentBinding,CreatorsViewModel>() {
+class CreatorFragment : BaseFragment<CreatorSeriesFragmentBinding,CreatorViewModel>() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,12 +16,12 @@ class CreatorsFragment : BaseFragment<CreatorSeriesFragmentBinding,CreatorsViewM
 
     override val layoutIdFragment: Int
         get() = R.layout.creator_series_fragment
-    override val viewModelClass: Class<CreatorsViewModel>
-        get() = CreatorsViewModel::class.java
+    override val viewModelClass: Class<CreatorViewModel>
+        get() = CreatorViewModel::class.java
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val adapter = CreatorAdapters(emptyList(), object : SeriesInterActionListener{})
+        val adapter = CreatorAdapter(emptyList(), object : SeriesInteractionListener{})
         binding.recyclerSeries.adapter = adapter
     }
 
--- app/src/main/java/com/abaferastech/marvelapp/ui/creators/CreatorViewModel.kt
+++ app/src/main/java/com/abaferastech/marvelapp/ui/creators/CreatorViewModel.kt
@@ -1,21 +1,17 @@
 package com.abaferastech.marvelapp.ui.creators
 
-import android.os.Parcelable.Creator
 import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import com.abaferastech.marvelapp.data.model.Comics
 import com.abaferastech.marvelapp.data.model.Creators
 import com.abaferastech.marvelapp.data.model.Series
-import com.abaferastech.marvelapp.data.model.Stories
 import com.abaferastech.marvelapp.data.model.response.MarvelResponse
 import com.abaferastech.marvelapp.data.model.state.State
 import com.abaferastech.marvelapp.data.repository.MarvelRepository
 import com.abaferastech.marvelapp.ui.base.BaseViewModel
-import io.reactivex.rxjava3.disposables.CompositeDisposable
 import io.reactivex.rxjava3.kotlin.addTo
 
-class CreatorsViewModel : BaseViewModel(), SeriesInterActionListener {
+class CreatorViewModel : BaseViewModel(), SeriesInteractionListener {
     private val repository = MarvelRepository()
 
     private val _creator = MutableLiveData<Creators>()
