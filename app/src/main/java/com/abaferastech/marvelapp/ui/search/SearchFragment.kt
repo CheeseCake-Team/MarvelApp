68c785fff9de61c4e0fac47179665ac5d3d65416
Abdurrahman <abdosalahas.01128@gmail.com>
2023-05-07 15:03:42 +0300

    create SearchFragment create SearchViewModel

---
+++ app/src/main/java/com/abaferastech/marvelapp/ui/search/SearchFragment.kt
@@ -0,0 +1,19 @@
+package com.abaferastech.marvelapp.ui.search
+
+import android.os.Bundle
+import android.view.View
+import com.abaferastech.marvelapp.R
+import com.abaferastech.marvelapp.databinding.FragmentSearchBinding
+import com.abaferastech.marvelapp.ui.base.BaseFragment
+
+class SearchFragment : BaseFragment<FragmentSearchBinding, SearchViewModel>() {
+    override val layoutIdFragment: Int
+        get() = R.layout.fragment_search
+    override val viewModelClass: Class<SearchViewModel>
+        get() = SearchViewModel::class.java
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
