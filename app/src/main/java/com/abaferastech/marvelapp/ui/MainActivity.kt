f0e72905df99ab94c0a6b57941cd33469ed3df98
hassanwasfy <wasfyelgamed@gmail.com>
2023-05-08 15:46:18 +0300

    add toolbar configurations and implement navigation

--- app/src/main/java/com/abaferastech/marvelapp/ui/MainActivity.kt
+++ app/src/main/java/com/abaferastech/marvelapp/ui/MainActivity.kt
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.abaferastech.marvelapp.R
 import com.abaferastech.marvelapp.data.local.SharedPreferencesServicesImpl
@@ -12,20 +15,31 @@ import com.abaferastech.marvelapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        setSupportActionBar(binding.toolbar)
         val navHostFragment = supportFragmentManager
             .findFragmentById(R.id.fragmentContainerView) as NavHostFragment
         val controller = navHostFragment.navController
         binding.bottomNavigationBar.setupWithNavController(controller)
         SharedPreferencesServicesImpl
             .initSharedPreferences(applicationContext)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(false)
+
+        setSupportActionBar(binding.toolbar)
+
+        appBarConfiguration = AppBarConfiguration(controller.graph)
+        setupActionBarWithNavController(controller, appBarConfiguration)
+
     }
+
 
     override fun onSupportNavigateUp(): Boolean {
-        findNavController(R.id.fragmentContainerView).navigateUp()
-        return true
+        val navController = findNavController(R.id.fragmentContainerView)
+        return navController.navigateUp(appBarConfiguration)
     }
-
 }
