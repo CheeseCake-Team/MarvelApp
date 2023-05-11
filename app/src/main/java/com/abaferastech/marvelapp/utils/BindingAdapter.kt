81af67e3ae9dc94ff72738dc57ef33e629e8ccb3
Abdurrahman <abdosalahas.01128@gmail.com>
2023-05-11 15:35:38 +0300

    add setSearchRecyclerViewItems and setAdapterByType methods

--- app/src/main/java/com/abaferastech/marvelapp/utils/BindingAdapter.kt
+++ app/src/main/java/com/abaferastech/marvelapp/utils/BindingAdapter.kt
@@ -10,11 +10,21 @@ import androidx.recyclerview.widget.RecyclerView
 import com.abaferastech.marvelapp.ui.model.DataItem
 import com.abaferastech.marvelapp.data.model.response.Thumbnail
 import com.abaferastech.marvelapp.data.model.result.Characters
+import com.abaferastech.marvelapp.data.model.result.Events
+import com.abaferastech.marvelapp.data.model.result.Series
 import com.abaferastech.marvelapp.ui.base.BaseAdapter
 import com.abaferastech.marvelapp.ui.characters.CharactersAdapter
+import com.abaferastech.marvelapp.ui.characters.CharactersInteractionListener
+import com.abaferastech.marvelapp.ui.comic.comics.ComicsAdapter
+import com.abaferastech.marvelapp.ui.comic.comics.ComicsInteractionListener
+import com.abaferastech.marvelapp.ui.events.EventAdapter
+import com.abaferastech.marvelapp.ui.events.EventsInteractionListener
 import com.abaferastech.marvelapp.ui.home.HomeFragmentDirections
 import com.abaferastech.marvelapp.ui.home.adapters.ComicAdapter
 import com.abaferastech.marvelapp.ui.home.adapters.SeriesAdapter
+import com.abaferastech.marvelapp.ui.home.adapters.SeriesInteractionListener
+import com.abaferastech.marvelapp.ui.model.SearchItem
+import com.abaferastech.marvelapp.ui.model.TYPE
 import com.abaferastech.marvelapp.ui.model.UIState
 import com.bumptech.glide.Glide
 
@@ -25,11 +35,65 @@ fun imageUrl (view: ImageView, thumbnail: Thumbnail?){
         .into(view)
 }
 
-
-
 @BindingAdapter(value = ["app:items"])
 fun <T> setRecyclerItems(view: RecyclerView, items: List<T>?) {
     (view.adapter as BaseAdapter<T>?)?.setItems(items ?: emptyList())
+}
+
+@BindingAdapter(value = ["app:setSearchRecyclerViewItems"])
+fun setSearchRecyclerViewItems(view: RecyclerView, items: SearchItem?) {
+    items.let {
+        when (items) {
+            is SearchItem.Character -> {
+                (view.adapter as CharactersAdapter?)?.setItems(items.items)
+            }
+            is SearchItem.Event -> {
+                (view.adapter as EventAdapter?)?.setItems(items.items)
+            }
+            is SearchItem.Series -> {
+                (view.adapter as SeriesAdapter?)?.setItems(items.items)
+            }
+            is SearchItem.Comic -> {
+                (view.adapter as ComicsAdapter?)?.setItems(items.items)
+            }
+            else -> {
+
+            }
+        }
+    }
+}
+
+@BindingAdapter(value = ["app:setAdapterByType"])
+fun setAdapterByType(view: RecyclerView, type: TYPE?) {
+    type.let {
+        val adapter = when (it){
+            TYPE.CHARACTER -> {
+                CharactersAdapter(emptyList(), object : CharactersInteractionListener {
+                    override fun onClickCharacter(character: Characters) {
+
+                    }
+                })
+            }
+            TYPE.EVENT -> {
+                EventAdapter(emptyList(), object : EventsInteractionListener {
+                    override fun onEventClick(event: Events) {
+
+                    }
+                })
+            }
+            TYPE.SERIES -> {
+                SeriesAdapter(emptyList(), object : SeriesInteractionListener {
+                    override fun onClickSeries(series: Series) {
+
+                    }
+                })
+            }
+            else -> {
+                ComicsAdapter(emptyList(), object : ComicsInteractionListener {})
+            }
+        }
+        view.adapter= adapter
+    }
 }
 
 
