48f36f58b4fb5dc69bca57e4806e93e594a120de
Anwaar Raafat <raafatanwaar@gmail.com>
2023-05-11 18:40:56 +0300

    add creator details fragment and CreatorDetailsViewModel and CreatorFragmentPageAdapter

---
+++ app/src/main/java/com/abaferastech/marvelapp/ui/creatorsDetails/CreatorDetailsViewModel.kt
@@ -0,0 +1,30 @@
+package com.abaferastech.marvelapp.ui.creatorsDetails
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.abaferastech.marvelapp.data.model.result.Creators
+import com.abaferastech.marvelapp.data.repository.MarvelRepository
+import com.abaferastech.marvelapp.ui.base.BaseViewModel
+import com.abaferastech.marvelapp.ui.model.UIState
+import io.reactivex.rxjava3.kotlin.addTo
+
+class CreatorDetailsViewModel : BaseViewModel() {
+    private val repository by lazy { MarvelRepository() }
+
+    private val _creator = MutableLiveData<UIState<Creators>>()
+    val creator: LiveData<UIState<Creators>> get() = _creator
+
+    fun getMarvelCreator(id: Int) {
+        repository.getSingleCreator(id)
+            .applySchedulersAndPostUIStates(_creator::postValue)
+    }
+
+}
+
+
+
+
+
+
+
+
---
+++ app/src/main/java/com/abaferastech/marvelapp/ui/creatorsDetails/CreatorFragmentPageAdapter.kt
@@ -0,0 +1,26 @@
+package com.abaferastech.marvelapp.ui.creatorsDetails
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.abaferastech.marvelapp.ui.comic.comics.ComicsViewAllHorizontalFragment
+import com.abaferastech.marvelapp.ui.events.EventsFragment
+import com.abaferastech.marvelapp.ui.model.TYPE
+
+class CreatorFragmentPageAdapter (
+    fragmentManager: FragmentManager,
+    lifecycle: androidx.lifecycle.Lifecycle,
+    private val id: Int
+) : FragmentStateAdapter(fragmentManager, lifecycle) {
+    override fun getItemCount(): Int {
+        return 2
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        return if (position == 0)
+            ComicsViewAllHorizontalFragment.newInstance(id, TYPE.CREATOR)
+        else {
+            EventsFragment.newInstance(id, TYPE.CREATOR)
+        }
+    }
+}
