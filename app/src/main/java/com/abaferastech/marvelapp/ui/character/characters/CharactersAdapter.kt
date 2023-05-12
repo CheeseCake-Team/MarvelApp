a098dcbaa542a49ded7bbd237928f265a4633426
Abdurrahman <abdosalahas.01128@gmail.com>
2023-05-12 18:37:19 +0300

    refactor characters fragment ui

--- app/src/main/java/com/abaferastech/marvelapp/ui/character/characters/CharactersAdapter.kt
+++ app/src/main/java/com/abaferastech/marvelapp/ui/character/characters/CharactersAdapter.kt
@@ -3,11 +3,10 @@ package com.abaferastech.marvelapp.ui.character.characters
 import com.abaferastech.marvelapp.R
 import com.abaferastech.marvelapp.data.model.result.Characters
 import com.abaferastech.marvelapp.ui.base.BaseAdapter
-import com.abaferastech.marvelapp.ui.base.BaseInteractionListener
 
 
 class CharactersAdapter(items: List<Characters>, charactersInteractionListener: CharactersInteractionListener) :
     BaseAdapter<Characters>(items, charactersInteractionListener) {
     override val layoutID: Int
-        get() = R.layout.item_character
+        get() = R.layout.item_view_all_characters
 }
