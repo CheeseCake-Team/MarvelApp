da4807973c4620796d0b251f32c15a62354ba3be
Mujtaba Mohammed <mojtabamohamd92@gmail.com>
2023-05-13 16:00:08 +0300

    fix Character Data Fragment and Creator Data Fragment

--- app/src/main/java/com/abaferastech/marvelapp/ui/creator/creatorsDetails/CreatorDataFragment.kt
+++ app/src/main/java/com/abaferastech/marvelapp/ui/creator/creatorsDetails/CreatorDataFragment.kt
@@ -3,13 +3,13 @@ package com.abaferastech.marvelapp.ui.creator.creatorsDetails
 import android.os.Bundle
 import android.view.View
 import com.abaferastech.marvelapp.R
-import com.abaferastech.marvelapp.databinding.FragmentComicDataBinding
+import com.abaferastech.marvelapp.databinding.FragmentCreatorDataBinding
 import com.abaferastech.marvelapp.ui.base.BaseFragment
 import com.abaferastech.marvelapp.ui.model.TYPE
 import com.abaferastech.marvelapp.utils.Constants
 
 
-class CreatorDataFragment : BaseFragment<FragmentComicDataBinding, CreatorDetailsViewModel>() {
+class CreatorDataFragment : BaseFragment<FragmentCreatorDataBinding, CreatorDetailsViewModel>() {
     override val layoutIdFragment: Int
         get() = R.layout.fragment_creator_data
     override val viewModelClass: Class<CreatorDetailsViewModel>
