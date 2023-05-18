95758497c41644250aea6c64bdff20d71484d575
Ziad mahmoud <ziaadmahmoud2001@hotmail.com>
2023-05-18 22:07:24 +0300

    refactor series single

--- app/src/main/java/com/abaferastech/marvelapp/ui/series/seriesDetails/SeriesDetailsViewModel.kt
+++ app/src/main/java/com/abaferastech/marvelapp/ui/series/seriesDetails/SeriesDetailsViewModel.kt
@@ -3,27 +3,29 @@ package com.abaferastech.marvelapp.ui.series.seriesDetails
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.SavedStateHandle
+import com.abaferastech.marvelapp.data.domain.models.Series
+import com.abaferastech.marvelapp.data.mapper.dtotodomain.ComicMapper
+import com.abaferastech.marvelapp.data.mapper.dtotodomain.SeriesMapper
 import com.abaferastech.marvelapp.data.remote.response.SeriesDTO
 import com.abaferastech.marvelapp.data.repository.MarvelRepository
 import com.abaferastech.marvelapp.ui.base.BaseViewModel
 import com.abaferastech.marvelapp.ui.model.UIState
 import javax.inject.Inject
 
-class SeriesDetailsViewModel
-@Inject constructor(private val repository: MarvelRepository)
-    : BaseViewModel() {
+class SeriesDetailsViewModel @Inject constructor(private val repository: MarvelRepository) :
+    BaseViewModel() {
 
-    private val _series = MutableLiveData<UIState<SeriesDTO>>()
-    val series: LiveData<UIState<SeriesDTO>> get() = _series
+    private val _series = MutableLiveData<Series>()
+    val series: LiveData<Series> get() = _series
 
-    /*val seriesArgs = state.let {
-        SeriesDetailsFragmentArgs.fromSavedStateHandle(it)
-    }*/
 
-    /*fun getSeriesById(passeId: Int? = null){
-        val seriesId = passeId ?: seriesArgs.seriesId
-        repository.getSingleSeries(seriesId)
-            .applySchedulersAndPostUIStates(_series::postValue)
-    }*/
+    var seriesId = MutableLiveData<Int>()
+
+    fun getSeriesById(passeId: Int) {
+        repository.getSingleSeries(passeId)
+            .applySchedulersAndPostUIStates {
+                _series.postValue(SeriesMapper().map(it.toData()!!))
+            }
+    }
 
 }
