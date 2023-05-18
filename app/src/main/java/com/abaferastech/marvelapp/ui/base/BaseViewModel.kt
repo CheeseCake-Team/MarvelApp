dd715e5b716a9a28d9eeb2d1cbd681c4b263ec1d
hassanwasfy <wasfyelgamed@gmail.com>
2023-05-18 11:53:40 +0300

    implement save instance state

--- app/src/main/java/com/abaferastech/marvelapp/ui/base/BaseViewModel.kt
+++ app/src/main/java/com/abaferastech/marvelapp/ui/base/BaseViewModel.kt
@@ -1,5 +1,6 @@
 package com.abaferastech.marvelapp.ui.base
 
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import com.abaferastech.marvelapp.ui.model.UIState
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
@@ -10,9 +11,17 @@ import io.reactivex.rxjava3.disposables.CompositeDisposable
 import io.reactivex.rxjava3.kotlin.addTo
 import io.reactivex.rxjava3.schedulers.Schedulers
 
-abstract class BaseViewModel : ViewModel() {
+abstract class BaseViewModel(private val savedStateHandle: SavedStateHandle) : ViewModel() {
 
     protected val compositeDisposable = CompositeDisposable()
+
+    protected fun <T : Any> getSavedStateValue(key: String): T? {
+        return savedStateHandle[key]
+    }
+
+    protected fun <T : Any> setSavedStateValue(key: String, value: T) {
+        savedStateHandle[key] = value
+    }
 
     protected fun <T : Any> Single<UIState<T>>.applySchedulersAndPostUIStates(
         postValue: (UIState<T>) -> Unit,
