d78d236e81ed3b5e5898b34b038439a9477265ba
Mujtaba Mohammed <mojtabamohamd92@gmail.com>
2023-05-18 23:01:06 +0300

    fix creator mapper

--- app/src/main/java/com/abaferastech/marvelapp/domain/mapper/CreatorMapper.kt
+++ app/src/main/java/com/abaferastech/marvelapp/domain/mapper/CreatorMapper.kt
@@ -1,17 +1,17 @@
 package com.abaferastech.marvelapp.domain.mapper
 
-import com.abaferastech.marvelapp.domain.mapper.IMapper
-import com.abaferastech.marvelapp.domain.models.Creator
-import com.abaferastech.marvelapp.data.local.database.entity.CreatorEntity
 import com.abaferastech.marvelapp.data.remote.response.CreatorDTO
+import com.abaferastech.marvelapp.domain.models.Creator
 
-class CreatorMapper: IMapper<CreatorEntity, Creator> {
-    override fun map(input: CreatorEntity): Creator {
-        return Creator(
-            id = input.id,
-            fullName = input.fullName,
-            modified = input.modified,
-            imageUri = input.imageUri
-        )
+class CreatorMapper : IMapper<CreatorDTO, Creator> {
+    override fun map(input: List<CreatorDTO>): List<Creator> {
+        return input.map { CreatorDTO ->
+            Creator(
+                id = CreatorDTO.id,
+                fullName = CreatorDTO.fullName,
+                modified = CreatorDTO.modified,
+                imageUri = "${CreatorDTO.thumbnail?.path}.${CreatorDTO.thumbnail?.extension}"
+            )
+        }
     }
 }
