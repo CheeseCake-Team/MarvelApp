84831367e09e72b2a2da0724fb180fea5b04149c
Rehammashal7 <rehammashal7@gmail.com>
2023-05-19 15:47:51 +0300

    create repo module

--- app/src/main/java/com/abaferastech/marvelapp/di/MarvelRepositoryModule.kt
+++ app/src/main/java/com/abaferastech/marvelapp/di/MarvelRepositoryModule.kt
@@ -2,23 +2,37 @@ package com.abaferastech.marvelapp.di
 
 import com.abaferastech.marvelapp.data.local.database.daos.CharacterDao
 import com.abaferastech.marvelapp.data.remote.MarvelApiService
+import com.abaferastech.marvelapp.data.repository.IMarvelRepository
 import com.abaferastech.marvelapp.data.repository.MarvelRepository
+import com.abaferastech.marvelapp.domain.mapper.*
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ViewModelComponent
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
 
 @Module
-@InstallIn(SingletonComponent::class)
+@InstallIn(ViewModelComponent::class)
 object MarvelRepositoryModule {
 
     @Provides
     @Singleton
-    fun provideMarvelRepository(characterDao: CharacterDao, apiService: MarvelApiService)
-            : MarvelRepository {
-        return MarvelRepository(characterDao, apiService)
+    fun provideMarvelRepository(apiService: MarvelApiService,
+                                comicMapper: ComicDominMapper,
+                                seriesMapper: SeriesMapper,
+                                creatorMapper: CreatorMapper,
+                                eventMapper: EventMapper,
+                                characterDomainMapper: CharacterDomainMapper
+    ): IMarvelRepository {
+        return MarvelRepository(
+            apiService,
+            comicMapper,
+            seriesMapper,
+            creatorMapper,
+            eventMapper,
+            characterDomainMapper
+        )
     }
-
 }
